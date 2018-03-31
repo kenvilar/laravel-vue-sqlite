0818346d508c65c86d68335bcaf5d54daa541361
Ken Vilar <kenvilar@gmail.com>
2018-03-31 16:38:56 +0800

    Add Contact.vue

---
+++ resources/assets/js/components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+    </h1>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            //
+        },
+        mounted: function () {
+            console.log('Contacts Components Loaded!');
+        }
+    }
+</script>
