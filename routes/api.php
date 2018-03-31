f6b96468c75afb31e116cf2d8354e831bf47e938
Ken Vilar <kenvilar@gmail.com>
2018-03-31 16:04:02 +0800

    Pull all data and single datum

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,19 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Contact;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['middleware' => 'api'], function () {
+    //Pull all data with latest contacts
+    Route::get('contacts', function() {
+        return Contact::query()->latest()->orderBy('created_at', 'desc')->get();
+    });
+
+    //Pull single contact
+    Route::get('contact/{id}', function ($id) {
+        return Contact::query()->findOrFail($id);
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
